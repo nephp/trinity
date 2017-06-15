3f000d20300324fb995a2ad78d5a9d7c89a93f33
Trinity Works <nephp@users.noreply.github.com>
2017-06-15 02:50:00 -0400

    Updated comments

--- app/classes/cookie.class.php
+++ app/classes/cookie.class.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * @link      https://github.com/nephp/trinity for the canonical source repository
+ * @project App\r7r1n17y\Framework (Trinity Framework)
  * @copyright Copyright (c) 2017-2017 nephp. (https://github.com/nephp)
- * @license   https://github.com/nephp/trinity/blob/master/LICENSE MIT License
  */
  
 namespace App\r7r1n17y\Framework;
