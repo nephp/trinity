b118d6222f801d521638759b57e8878f04e883be
Trinity Works <nephp@users.noreply.github.com>
2017-06-15 02:07:35 -0400

    Create the frameworks custom namespace

--- app/classes/cookie.class.php
+++ app/classes/cookie.class.php
@@ -1,1 +1,8 @@
-
+<?php
+/**
+ * @link      https://github.com/nephp/trinity for the canonical source repository
+ * @copyright Copyright (c) 2017-2017 nephp. (https://github.com/nephp)
+ * @license   https://github.com/nephp/trinity/blob/master/LICENSE MIT License
+ */
+ 
+namespace App\r7r1n17y\Framework;
